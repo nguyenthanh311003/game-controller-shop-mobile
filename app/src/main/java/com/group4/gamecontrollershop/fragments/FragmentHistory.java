2b58b781ea2cb2475ef5966b7a118dc3b689f933
Hai Dang <contact@hdang09.tech>
2024-10-19 14:37:50 +0700

    feat: complete history feature

--- app/src/main/java/com/group4/gamecontrollershop/fragments/FragmentHistory.java
+++ app/src/main/java/com/group4/gamecontrollershop/fragments/FragmentHistory.java
@@ -1,5 +1,6 @@
 package com.group4.gamecontrollershop.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,60 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.group4.gamecontrollershop.R;
+import com.group4.gamecontrollershop.adapter.HistoryAdapter;
+import com.group4.gamecontrollershop.model.Order;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class FragmentHistory extends Fragment {
+
+    private RecyclerView recyclerView;
+    private HistoryAdapter historyAdapter;
+    private List<Order> orderList;
+
+    @SuppressLint("SimpleDateFormat")
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        recyclerView = view.findViewById(R.id.recycleView);
+
+        Date orderDate = null;
+        try {
+            orderDate = dateFormat.parse("2023-08-15");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        orderList = new ArrayList<>();
+
+        Order order1 = new Order(1, 1, 1500000, orderDate, "success", null);
+        Order order2 = new Order(2, 1, 1500000, orderDate, "success", null);
+        Order order3 = new Order(3, 1, 1500000, orderDate, "success", null);
+        Order order4 = new Order(4, 1, 1500000, orderDate, "failure", null);
+        Order order5 = new Order(5, 1, 1500000, orderDate, "success", null);
+
+        orderList.add(order1);
+        orderList.add(order2);
+        orderList.add(order3);
+        orderList.add(order4);
+        orderList.add(order5);
+
+        historyAdapter = new HistoryAdapter(orderList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(historyAdapter);
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/group4/gamecontrollershop/model/Order.java
@@ -0,0 +1,74 @@
+package com.group4.gamecontrollershop.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private int id;
+    private int userId;
+//    private User user;
+    private int totalAmount;
+    private Date orderDate;
+    private String status;
+    private List<OrderItem> orderItems;
+
+    public Order() {
+    }
+
+    public Order(int id, int userId, int totalAmount, Date orderDate, String status, List<OrderItem> orderItems) {
+        this.id = id;
+        this.userId = userId;
+        this.totalAmount = totalAmount;
+        this.orderDate = orderDate;
+        this.status = status;
+        this.orderItems = orderItems;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+}
